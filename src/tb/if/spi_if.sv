05802b7370217804b82342d816ddf77612033381
Matheus Andrade de Almeida <matandalmeida@gmail.com>
2025-02-01 16:36:56 -0300

    Update spi_if.sv

--- src/tb/if/spi_if.sv
+++ src/tb/if/spi_if.sv
@@ -1,17 +1,30 @@
-interface spi_if(input logic clk, reset_n);
-    // SPI signals
-    logic cs_n;
-    logic mosi;
-    logic sclk;
-    logic mode;
-    logic [7:0] reg_addr;
-    logic reg_write;
-    logic [7:0] reg_wdata;
-    logic [7:0] reg_rdata;
-    logic miso;
-    logic ready;
+// Arquivo: spi_interface.sv
+interface spi_interface(input logic clk);
+  logic       reset_n;
+  logic       cs_n;
+  logic       mosi;
+  logic       miso;
+  logic       sclk;
+  logic       mode;
+  logic [7:0] reg_addr;
+  logic       reg_write;
+  logic [7:0] reg_wdata;
+  logic [7:0] reg_rdata;
+  logic       ready;
 
-    // Modports for Master and Slave
-    modport master(input clk, reset_n, cs_n, mosi, sclk, mode, reg_addr, reg_write, reg_wdata, output reg_rdata, miso, ready);
-    modport slave(input clk, reset_n, cs_n, sclk, mode, reg_addr, reg_write, reg_wdata, output reg_rdata, mosi, miso, ready);
+  // Modports para Master e Slave
+  modport master (output sclk, cs_n, input mosi, output miso);
+  modport slave (input sclk, cs_n, mosi, output miso);
+
+  // Clocking blocks para sincronização
+  clocking cb_master @(posedge clk);
+    output reset_n, cs_n, mosi, sclk, mode;
+    input miso, ready, reg_rdata;
+  endclocking
+
+  clocking cb_slave @(posedge clk);
+    input reset_n, cs_n, mosi, sclk, mode;
+    output miso;
+    input ready, reg_rdata;
+  endclocking
 endinterface
