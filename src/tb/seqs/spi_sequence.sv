26169796d4cd5afcb791fc2e28a748de9345b668
Matheus Andrade de Almeida <matandalmeida@gmail.com>
2025-02-01 16:41:18 -0300

    Create spi_sequence.sv

---
+++ src/tb/seqs/spi_sequence.sv
@@ -0,0 +1,18 @@
+// Arquivo: spi_sequence.sv
+class spi_sequence extends uvm_sequence #(spi_transaction);
+  `uvm_object_utils(spi_sequence)
+
+  function new(string name = "spi_sequence");
+    super.new(name);
+  endfunction
+
+  virtual task body();
+    spi_transaction tr;
+    repeat(10) begin
+      tr = spi_transaction::type_id::create("tr");
+      start_item(tr);
+      assert(tr.randomize());
+      finish_item(tr);
+    end
+  endtask
+endclass
