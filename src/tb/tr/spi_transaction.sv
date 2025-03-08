190ca65eb3decfa13789cf6eecce3c5c448bdc16
Matheus Andrade de Almeida <matandalmeida@gmail.com>
2025-03-08 16:38:01 -0300

    Update spi_transaction.sv

--- src/tb/tr/spi_transaction.sv
+++ src/tb/tr/spi_transaction.sv
@@ -1,18 +1,23 @@
-// Arquivo: spi_transaction.sv
 class spi_transaction extends uvm_sequence_item;
-  rand bit [7:0] data;
-  rand int       clock_div;
-  rand bit       mode;       // 0: Slave, 1: Master
-  rand bit       lsb_first;
+    rand bit [7:0] data;
+    rand bit [7:0] received_data; // Added received_data
+    rand int       clock_div;
+    rand bit       mode;
+    rand bit       lsb_first;
+    rand time      duration;      // Added duration
+    rand bit [1:0] error_code;    // Added error_code
 
-  `uvm_object_utils_begin(spi_transaction)
-    `uvm_field_int(data, UVM_ALL_ON)
-    `uvm_field_int(clock_div, UVM_ALL_ON)
-    `uvm_field_int(mode, UVM_ALL_ON)
-    `uvm_field_int(lsb_first, UVM_ALL_ON)
-  `uvm_object_utils_end
+    `uvm_object_utils_begin(spi_transaction)
+        `uvm_field_int(data, UVM_ALL_ON)
+        `uvm_field_int(received_data, UVM_ALL_ON)
+        `uvm_field_int(clock_div, UVM_ALL_ON)
+        `uvm_field_int(mode, UVM_ALL_ON)
+        `uvm_field_int(lsb_first, UVM_ALL_ON)
+        `uvm_field_int(duration, UVM_ALL_ON)
+        `uvm_field_int(error_code, UVM_ALL_ON)
+    `uvm_object_utils_end
 
-  function new(string name = "spi_transaction");
-    super.new(name);
-  endfunction
+    function new(string name = "spi_transaction");
+        super.new(name);
+    endfunction
 endclass
